ce499f41d62da9f8ae9daec9a99d6491a2207a16
1f604 <1f604@users.noreply.github.com>
2019-01-21 11:34:01 +0000

    add PIXI.js method for detecting webGL

--- frontend/game.js
+++ frontend/game.js
@@ -59,6 +59,43 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function isWebGLSupported()
+{
+    const contextOptions = { stencil: true, failIfMajorPerformanceCaveat: true };
+
+    try
+    {
+        if (!window.WebGLRenderingContext)
+        {
+            return false;
+        }
+
+        const canvas = document.createElement('canvas');
+        let gl = canvas.getContext('webgl', contextOptions) || canvas.getContext('experimental-webgl', contextOptions);
+
+        const success = !!(gl && gl.getContextAttributes().stencil);
+
+        if (gl)
+        {
+            const loseContext = gl.getExtension('WEBGL_lose_context');
+
+            if (loseContext)
+            {
+                loseContext.loseContext();
+            }
+        }
+
+        gl = null;
+
+        return success;
+    }
+    catch (e)
+    {
+        return false;
+    }
+}
+
+
 function create ()
 {
     function sendKeyPress (keypress)
